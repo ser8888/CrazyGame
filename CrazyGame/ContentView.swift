7ad2658da27ddd5c7960626f3dfd56d568654940
Sergejs Tiselskis <slokas85@gmail.con>
2022-09-20 20:53:55 +0300

    Initial Commit

---
+++ CrazyGame/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CrazyGame
+//
+//  Created by John Doe on 20/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CrazyGame/CrazyGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrazyGameApp.swift
+//  CrazyGame
+//
+//  Created by John Doe on 20/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct CrazyGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
